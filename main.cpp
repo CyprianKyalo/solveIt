ef30f1290a4ec62637ea24ed2074c219b2fadd88
Cyprian Kyalo <cypkyalo17@gmail.com>
2022-01-25 21:16:21 +0300

    Mouse Input

--- main.cpp
+++ main.cpp
@@ -23,8 +23,11 @@
 using namespace std;
 
 int j = 0;
+int key = 0;
 int refpoint = 1;
 int selpoint = 1;
+int score_value = 0;
+string sc = "";
 
 void enterController(int& refpoint, int& selpoint);
 
@@ -50,11 +53,47 @@ void multiplication_quiz();
 void division_quiz();
 void display_sub(GLFWwindow* window);
 
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		//cout << "I ws pressed" << endl;
+		double xpos, ypos;
+		//getting cursor position
+		glfwGetCursorPos(window, &xpos, &ypos);
 
+		cout << "Cursor Position at (" << xpos << " : " << ypos << ")" << endl;
+		if (key == 0 && xpos >= 300.0 && xpos <= 500.0 && ypos >= 100.0 && ypos <= 300.0)
+		{
+			cout << "Correct" << endl;
+			score_value = score_value + 10;
+			cout << "Score is " << score_value << endl;		
+		}
+		else if (key == 1 && xpos >= 300.0 && xpos <= 500.0 && ypos >= 400.0 && ypos <= 600.0) {
+			cout << "Correct" << endl;
+			score_value = score_value + 10;
+			cout << "Score is " << score_value << endl;
+		}
+		else if (key == 2 && xpos >= 600.0 && xpos <= 800.0 && ypos >= 100.0 && ypos <= 300.0) {
+			cout << "Correct" << endl;
+			score_value = score_value + 10;
+			cout << "Score is " << score_value << endl;
+		}
+		else if (key == 3 && xpos >= 600.0 && xpos <= 800.0 && ypos >= 400.0 && ypos <= 600.0) {
+			cout << "Correct" << endl;
+			score_value = score_value + 10;
+			cout << "Score is " << score_value << endl;
+		}
+		else {
+			cout << "Wrong nswer" << endl;
+			score_value = score_value - 10;
+			cout << "Score is " << score_value << endl;
+			//exit(0);
+		}
+	}
+}
 
 int main()
 {
-
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -100,15 +139,6 @@ int main()
 
 	// FreeType
 	TextRenderer txt("Fonts/Antonio-Bold.ttf");
-	/*int *p;
-	p = addition_quiz();*/
-	//addition_quiz();
-
-	/*for (int i = 0; i < 5; i++)
-	{
-		cout << "The vlue is " << *(p + 1) << endl;
-	}*/
-
 
 	/*Addition d;
 	cout << "This is opernd 1 " << d.A << endl;
@@ -137,92 +167,23 @@ int main()
 			glfwSetWindowShouldClose(window, true);
 			break;
 		case 1:
-			display(window);
+			txt.RenderText(shader, "Welcome to solveIt", 250.0f, 700.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
 			break;
 		case 2:
+			display(window);
+			break;
+		case 3:
 			display_sub(window);
 			break;
-			/*switch (selpoint)
-			{
-			case 1:
-				txt.RenderText(shader, "Level Addition", 540.0f, 250.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-				break;
-			case 2:
-				txt.RenderText(shader, "Level Subtraction", 540.0f, 250.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-				break;
-			default:
-				break;
-			}*/
+		case 4:
+			sc += to_string(score_value);
+			//txt.RenderText(shader, "Your score is ", 500.0f, 700.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
+			txt.RenderText(shader, sc, 700.0f, 700.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
+			break;
+
 		default:
 			break;
 		}
-		//switch (refpoint)
-		//{
-		//case 0:
-		//	glfwSetWindowShouldClose(window, true);
-		//	break;
-		//case 1:
-		//	txt.RenderText(shader, "Welcome to the Jungle", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
-		//	txt.RenderText(shader, "MATHTRIX", 440.0f, 570.0f, 1.0f, glm::vec3(0.3, 0.7f, 0.9f));
-		//	break;
-		//case 2:
-		//	txt.RenderText(shader, "Level Select", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//	/*switch (selpoint)
-		//	{
-		//	case 1:
-		//		txt.RenderText(shader, "Level Addition", 540.0f, 250.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//		break;
-		//	case 2:
-		//		txt.RenderText(shader, "Level Subtraction", 540.0f, 250.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//		break;
-		//	case 3:
-		//		txt.RenderText(shader, "Level Multiplication", 540.0f, 250.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//		break;
-		//	case 4:
-		//		txt.RenderText(shader, "Level Division", 540.0f, 250.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//		break;
-		//	case 5:
-		//		txt.RenderText(shader, "Level Prime Numbers", 540.0f, 250.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//		break;
-		//	case 6:
-		//		txt.RenderText(shader, "Level Divisibility", 540.0f, 250.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//		break;
-		//	case 7:
-		//		txt.RenderText(shader, "Level Squares and Square Roots", 540.0f, 250.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//		break;
-		//	default:
-		//		break;
-		//	}
-		//	break;*/
-		//case 3:
-		//	txt.RenderText(shader, "Level Addition", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//	//Where Functions will be called that'll provide operands
-		//	break;
-		//case 4:
-		//	txt.RenderText(shader, "Level Subtraction", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//	//Where Functions will be called that'll provide operands
-		//	break;
-		//case 5:
-		//	txt.RenderText(shader, "Level Multiplication", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//	//Where Functions will be called that'll provide operands
-		//	break;
-		//case 6:
-		//	txt.RenderText(shader, "Level Division", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//	//Where Functions will be called that'll provide operands
-		//	break;
-		//case 7:
-		//	txt.RenderText(shader, "Level Prime Numbers", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//	//Where Functions will be called that'll provide operands
-		//	break;
-		//case 8:
-		//	txt.RenderText(shader, "Level Divisibility", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//	break;
-		//case 9:
-		//	txt.RenderText(shader, "Level Squares and Square Roots", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
-		//	break;
-		//default:
-		//	break;
-		//}
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -298,149 +259,11 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 
-//void mouse_button_callback(GLFWwindow* window)
-//{
-//	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
-//		//glfwSetWindowShouldClose(window, true);
-//		//cout << "I was pressed" << endl;
-//		double xpos, ypos;
-//		//getting cursor position
-//		glfwGetCursorPos(window, &xpos, &ypos);
-//		cout << "Cursor Position at (" << xpos << " : " << ypos << endl;
-//	}
-//}
-
 string ans1, ans2 = "";
 string p;
 int numInput, A, B;
 int score = 0;
 string p1, p2 = "";
-
-void trial(GLFWwindow *window)
-{
-	//glClear(GL_COLOR_BUFFER_BIT);
-
-	Shader shader("Shaders/glyph.vs", "Shaders/glyph.fs");
-	glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(SCR_WIDTH), 0.0f, static_cast<float>(SCR_HEIGHT));
-	shader.use();
-	glUniformMatrix4fv(glGetUniformLocation(shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-	// FreeType
-	TextRenderer txt("Fonts/Antonio-Bold.ttf");
-
-	txt.RenderText(shader, "Level 1 : Addition Quiz", 250.0f, 700.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-
-	int A, B;
-	time_t start = time(0);
-
-	std::cout << "\n LEVEL 1: Addition Quiz (YOU ONLY HAVE 30 SECONDS TO SOLVE EACH QUESTION) \n\n";
-	A = rand() % 10 + 1;
-	B = rand() % 10 + 1 + 10;
-	p1 = to_string(A);
-	p2 = to_string(B);
-	p += p1;
-	p += "+";
-	p += p2;
-	txt.RenderText(shader, p1, 50.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	txt.RenderText(shader, "+", 130.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	txt.RenderText(shader, p2, 200.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	
-
-	/*cin >> numInput;
-	if ((time(0) - start) > 30)
-	{
-		cout << "Time's up";
-		exit(0);
-	}
-	else if (numInput == A + B) {
-		cout << "Correct Answer\n";
-		score = score + 10;
-		cout << "Score : " << score << endl;
-	}
-	else {
-		cout << "Wrong Answer\n";
-		score = score - 10;
-		if (score < 0)
-			score = 0;
-		cout << "Score : " << score << endl;
-
-	}*/
-	//Beep(0, 4000);
-	//for (int i = 1; i <= 5; i++)
-	//{
-	//	srand(time(0));
-	//	A = rand() % 10 + 1 + 10 * (i - 1);
-	//	B = rand() % 10 + 1 + 10 * (i - 1);
-	//	cout << "Question " << i << " : " << A << " + " << B << " = \n";
-	//	//o1[i] = A;
-	//	int numInput;
-	//	time_t start = time(0);
-	//	int y = 30;
-	//	p1 = to_string(A);
-	//	p2 = to_string(B);
-	//	p += p1;
-	//	p += "+";
-	//	p += p2;
-	//	txt.RenderText(shader, p1, 50.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	//	txt.RenderText(shader, "+", 130.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	//	txt.RenderText(shader, p2, 200.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	//	while (!_kbhit())
-	//	{
-	//		if (abs(time(0) - start) > 30)
-	//		{
-	//			j = 1;
-	//			break;
-	//		}
-	//	}
-	//	if (j == 1)
-	//	{
-	//		cout << "Time's up";
-	//		exit(0);
-	//		//j = 2;
-	//	}
-	//	else
-	//	{
-	//		cin >> numInput;
-	//		if ((time(0) - start) > 30)
-	//		{
-	//			cout << "Time's up";
-	//			exit(0);
-	//			j = 2;
-	//		}
-	//		else if (numInput == A + B) {
-	//			cout << "Correct Answer\n";
-	//			score = score + 10;
-	//			cout << "Score : " << score << endl;
-	//			//cout << "op1 is : " << op1[i] << endl<< endl;
-	//		}
-	//		else {
-	//			cout << "Wrong Answer\n";
-	//			score = score - 10;
-	//			if (score < 0)
-	//				score = 0;
-	//			cout << "Score : " << score << endl;
-
-	//		}
-
-	//	}
-	//	if (j == 2) break;
-	//	{
-	//		Beep(2000, 100); Beep(1500, 100); Beep(2000, 100); Beep(1500, 100); Beep(2000, 700); Beep(0, 400);
-	//	}
-	//	if (i == 5)
-	//		break;
-
-	//	/*p1 = to_string(A);
-	//	p2 = to_string(B);
-	//	p += p1;
-	//	p += "+";
-	//	p += p2;
-	//	txt.RenderText(shader, "Level 1 : Addition Quiz", 50.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	//	txt.RenderText(shader, p1, 100.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	//	txt.RenderText(shader, "+", 130.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	//	txt.RenderText(shader, p2, 150.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-	//	break;*/
-	//}
-}
 
 /*Level 1*/
 void addition_quiz(GLFWwindow *window)
@@ -831,16 +654,6 @@ void division_quiz()
 	//}
 //}
 
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
-{
-	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
-		cout << "I ws pressed" << endl;
-		//double xpos, ypos;
-		////getting cursor position
-		//glfwGetCursorPos(window, &xpos, &ypos);
-		//cout << "Cursor Position at (" << xpos << " : " << ypos << endl;
-	}
-}
 
 static void cursorPositionCallback(GLFWwindow* window, double xPos, double yPos)
 {
@@ -858,15 +671,15 @@ void window_refresh_callback(GLFWwindow* window)
 }
 
 void display(GLFWwindow* window) {
+	glfwSwapInterval(250);
+
 	Shader shader("Shaders/glyph.vs", "Shaders/glyph.fs");
 	glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(SCR_WIDTH), 0.0f, static_cast<float>(SCR_HEIGHT));
 	shader.use();
 	glUniformMatrix4fv(glGetUniformLocation(shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
 	// FreeType
-	TextRenderer txt("Fonts/Antonio-Bold.ttf");
-
-	glfwSwapInterval(250);
+	TextRenderer txt("Fonts/Antonio-Bold.ttf");	
 
 	map<int, string> randAnswers;
 	int eqns[10], random;
@@ -874,7 +687,7 @@ void display(GLFWwindow* window) {
 	string op1, op2 = "";
 	Equation eqn;
 
-	txt.RenderText(shader, "Level 1 : Addition Quiz", 250.0f, 700.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
+	txt.RenderText(shader, "Level 1 : Addition Quiz", 400.0f, 700.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
 
 	eqn.generate_rand_nums(eqns);
 	for (unsigned int i = 0; i < 3; i += 2)
@@ -885,7 +698,7 @@ void display(GLFWwindow* window) {
 		q += op1;
 		q += "+";
 		q += op2;
-		cout << eqns[i] << " + " << eqns[i + 1] << " is " << ans << endl;
+		//cout << eqns[i] << " + " << eqns[i + 1] << " is " << ans << endl;
 
 		answer += to_string(ans);
 		answer1 += to_string(ans + 22);
@@ -925,19 +738,23 @@ void display(GLFWwindow* window) {
 			randAnswers[3] = answer;
 		}
 
+		
+		for (auto& i : randAnswers) {
+			if (i.second == answer) {
+				key = i.first;
+				break; // to stop searching
+			}
+		}
+		cout << "The answer is in position " << key <<endl;
 
+		txt.RenderText(shader, op1, 480.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
+		txt.RenderText(shader, "+", 550.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
+		txt.RenderText(shader, op2, 600.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
 
-		txt.RenderText(shader, op1, 50.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-		txt.RenderText(shader, "+", 100.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-		txt.RenderText(shader, op2, 130.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-
-		//std::string s;
-		//s += '1 'answer;
-
-		txt.RenderText(shader, randAnswers[0], 130.0f, 500.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-		txt.RenderText(shader, randAnswers[1], 130.0f, 400.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-		txt.RenderText(shader, randAnswers[2], 130.0f, 300.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-		txt.RenderText(shader, randAnswers[3], 130.0f, 200.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
+		txt.RenderText(shader, randAnswers[0], 400.0f, 200.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
+		txt.RenderText(shader, randAnswers[1], 400.0f, 500.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
+		txt.RenderText(shader, randAnswers[2], 700.0f, 200.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
+		txt.RenderText(shader, randAnswers[3], 700.0f, 500.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
 		break;
 	}
 }
@@ -1010,12 +827,9 @@ void display_sub(GLFWwindow* window) {
 			randAnswers[3] = answer;
 		}
 
-
-
 		txt.RenderText(shader, op1, 50.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
 		txt.RenderText(shader, "-", 100.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
 		txt.RenderText(shader, op2, 130.0f, 600.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
-
 		
 
 		txt.RenderText(shader, randAnswers[0], 130.0f, 500.0f, 1.0f, glm::vec3(0.2, 0.6f, 0.5f));
