92690047a51002bfb47668cadad632a09ef5f279
Bettyrosew <bettyrosaw@gmail.com>
2022-01-26 14:18:49 +0300

    Header file update

---
+++ addition.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef ADDITION_H
+#define ADDITION_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+#include "Equation.h"
+
+
+void display();
+void display_sub();
+void display_multiply();
+void display_division();
+#endif ADDITION_H
